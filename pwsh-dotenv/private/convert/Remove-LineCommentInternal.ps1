b33272ed651e32b210d4b7bb0ed2d28ae7d6b7a0
sawamura tsukasa <kamiyonanayo@booksakura.com>
2023-11-28 14:38:23 +0000

    fix issue #1

--- pwsh-dotenv/private/convert/Remove-LineCommentInternal.ps1
+++ pwsh-dotenv/private/convert/Remove-LineCommentInternal.ps1
@@ -4,20 +4,42 @@ Set-StrictMode -Version Latest
 function Remove-LineCommentInternal {
     [CmdletBinding()]
     [OutputType([string])]
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Scope='Function')]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Scope = 'Function')]
     Param(
         [string]$InputObject
     )
+    Begin {
+
+        function trimComment {
+            param ($str)
+            $str = $str.TrimStart()
+            if ("" -eq $str) {
+                return $str
+            }
+            if ("#" -ne $str[0]) {
+                return $str
+            }
+            # split newline
+            $first, $tail = Split-LineInternal $str
+            return $tail
+        }
+
+    }
     Process {
-        $str = $InputObject.TrimStart()
-        if ("" -eq $str) {
-            return $str
+        $str = $InputObject
+        if ($null -eq $str) {
+            $str = ""
         }
-        if ("#" -ne $str[0]) {
-            return $str
+        $prev_str = $str
+        while ($true) {
+            $str = trimComment $str
+            if ("" -eq $str) {
+                return $str;
+            }
+            if ($prev_str -eq $str) {
+                return $str;
+            }
+            $prev_str = $str
         }
-        # split newline
-        $first, $tail = Split-LineInternal $str
-        return $tail
     }
 }
